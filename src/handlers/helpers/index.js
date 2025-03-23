775973e066471e1bb1961c00084400f132976c9f
deafeningroar <finalx7@gmail.com>
2025-03-22 23:57:06 -0300

    Add missing import & further improve embed safe

--- src/handlers/helpers/index.js
+++ src/handlers/helpers/index.js
@@ -1,3 +1,5 @@
+const { hideLinkEmbed } = require('discord.js');
+
 const getFormattedMessage = message => {
   const [command, ...rest] = message.content.split(' ');
 
@@ -9,7 +11,7 @@ const getFormattedMessage = message => {
 
 const formatResponseMessage = message => {
   const result = message.replaceAll(/(http.*)/gi, substring => {
-    const embedSafe = hideLinkEmbed(substring.replaceAll(')', ''));
+    const embedSafe = hideLinkEmbed(substring.replaceAll(')', '').replaceAll(' ', ''));
     const parenthesisCount = substring.match(/\)/gi)?.length || 0;
 
     return `${embedSafe}${')'.repeat(parenthesisCount)}`;
