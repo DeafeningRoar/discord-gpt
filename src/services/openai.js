ebe28dabce10f92493715ac581c9c8e673a0b53a
deafeningroar <finalx7@gmail.com>
2025-03-30 00:09:27 -0300

    Improve logs

--- src/services/openai.js
+++ src/services/openai.js
@@ -6,7 +6,7 @@ const SEARCH_PREVIEW_MODEL = 'gpt-4o-mini-search-preview';
 const TEXT_MODEL = 'gpt-4o-mini';
 
 const webQuery = async message => {
-  console.log('Processing message with', SEARCH_PREVIEW_MODEL);
+  console.log(new Date().toISOString(), '- Processing message with', SEARCH_PREVIEW_MODEL);
   const response = await openai.chat.completions.create({
     model: SEARCH_PREVIEW_MODEL,
     messages: [
@@ -21,7 +21,7 @@ const webQuery = async message => {
 };
 
 const textQuery = async message => {
-  console.log('Processing message with', TEXT_MODEL);
+  console.log(new Date().toISOString(), '- Processing message with', TEXT_MODEL);
   const response = await openai.responses.create({
     model: TEXT_MODEL,
     input: message
