40864e30b63089fc45fe569e24bd2e587aba6192
deafeningroar <finalx7@gmail.com>
2025-03-31 22:42:03 -0300

    Add perplexity AI usage for web search queries

--- src/services/openai.js
+++ src/services/openai.js
@@ -1,29 +1,58 @@
 import OpenAI from 'openai';
 
 const openai = new OpenAI();
+const perplexityai = new OpenAI({
+  baseURL: 'https://api.perplexity.ai',
+  apiKey: process.env.PERPLEXITY_API_KEY
+});
 
-const SEARCH_PREVIEW_MODEL = 'gpt-4o-mini-search-preview';
-const TEXT_MODEL = 'gpt-4o-mini';
+const MODELS = {
+  OpenAI: {
+    SEARCH_PREVIEW_MODEL: 'gpt-4o-mini-search-preview',
+    TEXT_MODEL: 'gpt-4o-mini'
+  },
+  PerplexityAI: {
+    SONAR: 'sonar'
+  }
+};
 
-const webQuery = async message => {
-  console.log(new Date().toISOString(), '- Processing message with', SEARCH_PREVIEW_MODEL);
-  const response = await openai.chat.completions.create({
-    model: SEARCH_PREVIEW_MODEL,
-    messages: [
-      {
-        role: 'user',
-        content: message
+const formatPerplexityResponse = response => {
+  try {
+    const { citations } = response;
+
+    response.choices[0].message.content = response.choices[0].message.content.replaceAll(
+      /\[(\d+)\]/gm,
+      (substring, captureGroup) => {
+        const citation = citations[Number(captureGroup) - 1];
+        return `[${substring}](${citation})`;
       }
-    ]
-  });
+    );
+  } catch (error) {
+    console.log(new Date().toISOString(), 'Error formatting Perplexity response', {
+      message: error.message
+    });
+  }
 
   return response;
 };
 
+const webQuery = async message => {
+  console.log(new Date().toISOString(), '- Processing message with', MODELS.PerplexityAI.SONAR);
+  const response = await perplexityai.chat.completions.create({
+    model: MODELS.PerplexityAI.SONAR,
+    messages: [
+      { role: 'system', content: 'Be precise and concise.' },
+      { role: 'user', content: message }
+    ]
+  });
+
+  return formatPerplexityResponse(response);
+};
+
 const textQuery = async (message, img) => {
-  console.log(new Date().toISOString(), '- Processing message with', TEXT_MODEL);
+  console.log(new Date().toISOString(), '- Processing message with', MODELS.OpenAI.TEXT_MODEL);
   const response = await openai.responses.create({
-    model: TEXT_MODEL,
+    model: MODELS.OpenAI.TEXT_MODEL,
     input: [
       {
         role: 'user',
