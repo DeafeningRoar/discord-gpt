600b376c15f73220aa017ad60d757eb04d8de8de
deafeningroar <finalx7@gmail.com>
2025-07-12 03:51:27 -0400

    Fix embedding bug

--- src/handlers/helpers/response-formatters.ts
+++ src/handlers/helpers/response-formatters.ts
@@ -4,9 +4,9 @@ const embedCitations = (response: string, citations?: string[]): string => {
   try {
     if (!response || !citations?.length) return response;
 
-    return response.replaceAll(/\[(\d+)\]/gm, (substring, captureGroup) => {
+    return response.replaceAll(/\[{1,2}(\d+)\]{1,2}(?:\(([^)]+)\))?/gm, (_, captureGroup) => {
       const citation = citations[Number(captureGroup) - 1];
-      return `[${substring}](${citation})`;
+      return `[[${captureGroup}]](${citation})`;
     });
   } catch (error) {
     logger.error('Error formatting AI Response', {
