57f022d2a0d3d91e5a68f2bb4c0330092abe5fc1
deafeningroar <finalx7@gmail.com>
2025-08-13 18:21:14 -0400

    Added new internal service for facts feature

---
+++ src/services/internal.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+import { INTERNAL_BASE_URL, INTERNAL_API_KEY } from '../config/env';
+import logger from './logger';
+
+const api = axios.create({
+  baseURL: INTERNAL_BASE_URL,
+  params: {
+    uid: INTERNAL_API_KEY,
+  },
+});
+
+const getFacts = async (id: string) => {
+  try {
+    const { data } = await api.get<string>(`/facts/${id}`);
+
+    return data?.length ? data : undefined;
+  } catch (error: unknown) {
+    logger.error('Error fetching facts', error);
+  }
+};
+
+export default { getFacts };
