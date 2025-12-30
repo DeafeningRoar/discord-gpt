91b15c33db598cbbb9d16a80433399d0acec5bb1
deafeningroar <finalx7@gmail.com>
2025-12-30 17:54:00 -0300

    Add internal scheduler with job to process pending batches

--- src/events/index.ts
+++ src/events/index.ts
@@ -4,10 +4,12 @@ import DiscordListeners from './discord/listeners';
 import OpenAIListeners from './openai/listeners';
 import ProcessingPipelineListeners from './processing-pipeline';
 import DiscordScheduler from '../integrations/discord/scheduler';
+import InternalScheduler from '../integrations/internal/scheduler';
 
 export default ({ discord }: { discord?: Discord }) => {
   OpenAIListeners();
   ProcessingPipelineListeners();
+  InternalScheduler.start();
 
   if (discord) {
     DiscordListeners({ discord });
---
+++ src/integrations/internal/scheduler.ts
@@ -0,0 +1,98 @@
+import type { Conversation, Configuration } from '../../database/schemas';
+
+import { CronJob } from 'cron';
+import { Emitter, logger } from '../../services';
+
+import { mongoose, conversation, configuration } from '../../database';
+import { PIPELINE_EVENTS } from '../../config/constants';
+
+const autoStart = false;
+
+const pendingQueueWorker = new CronJob(
+  '*/1 * * * * *',
+  async function () {
+    try {
+      if (mongoose.client?.connection?.readyState !== 1) {
+        logger.info('Mongoose client not started, skipping pending queue check');
+        return;
+      }
+
+      const conversationModel = conversation.getModel();
+      const configsModel = configuration.getModel();
+
+      const speakConfigs = await configsModel.findOne<
+        Configuration<{ processingDelay: number; maxProcessingDelay: number }>
+      >({
+        name: 'conversation_settings',
+      });
+      const { processingDelay = 1000, maxProcessingDelay = 15000 } = speakConfigs?.config || {};
+
+      const ts = Date.now();
+      const documents = await conversationModel.find<Conversation>({
+        $and: [
+          { 'state.active': true },
+          { 'locks.pending': true },
+          {
+            $or: [
+              { 'state.lastUserMessageAt': { $lte: ts - processingDelay } },
+              { 'metadata.pendingStartAt': { $lte: ts - maxProcessingDelay } },
+            ],
+          },
+        ],
+      });
+
+      if (!documents.length) {
+        return;
+      }
+
+      logger.info(`Processing ${documents.length} conversations with pending messages`);
+
+      await Promise.all(
+        documents.map(async (doc) => {
+          const updatedDoc = await conversationModel.findOneAndUpdate(
+            {
+              _id: doc._id,
+              'locks.pending': true,
+              $or: [
+                { 'state.lastUserMessageAt': { $lte: ts - processingDelay } },
+                { 'metadata.pendingStartAt': { $lte: ts - maxProcessingDelay } },
+              ],
+            },
+            {
+              $set: {
+                'locks.pending': false,
+                'metadata.pendingStartAt': null,
+              },
+            },
+            { new: true },
+          );
+
+          if (updatedDoc) {
+            Emitter.emit(PIPELINE_EVENTS.DECISION_INPUT_PROCESSED, {
+              id: doc._id,
+              data: { id: doc.channelId },
+              context: { source: doc.source },
+              responseEvent: doc.metadata.responseEvent,
+            });
+          }
+        }),
+      );
+    } catch (error: unknown) {
+      const err = error as Error;
+
+      logger.error('Error while processing pending queues', {
+        message: err.message,
+        cause: err.cause,
+        stack: err.stack,
+      });
+    }
+  },
+  null,
+  autoStart,
+);
+
+export default {
+  start: () => {
+    pendingQueueWorker.start();
+  },
+};
